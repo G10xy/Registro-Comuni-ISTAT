68a7fb4e98834046c4f33bc36a40dac3436e2bed
gioxi <desiderio.gio@yahoo.com>
2023-04-30 02:12:36 +0200

    feat entity composite id

---
+++ src/main/java/it/municipalitiesregistry/persistence/entity/RegistryPlaceId.java
@@ -0,0 +1,32 @@
+package it.municipalitiesregistry.persistence.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Embeddable
+public class RegistryPlaceId implements Serializable {
+
+
+    @Column(name = "denominazione_regione")
+    private String denominazioneRegione;
+
+    @Column(name = "codice_catastale_del_comune")
+    private String codiceCatastaleDelComune;
+
+    @Column(name = "denominazione_in_italiano")
+    private String denominazioneInItaliano;
+
+    @Column(name = "denominazione_unita_territoriale_sovracomunale")
+    private String denominazioneUnitaTerritorialeSovracomunale;
+}
