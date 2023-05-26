dd0acbe1c74ef482c41dbfc9d67f8332873e31f8
gioxi <desiderio.gio@yahoo.com>
2023-05-27 00:40:49 +0200

    update mapper

--- src/main/java/it/municipalitiesregistry/mapper/RegistryPlaceMapper.java
+++ src/main/java/it/municipalitiesregistry/mapper/RegistryPlaceMapper.java
@@ -1,8 +1,8 @@
 package it.municipalitiesregistry.mapper;
 
 
-import it.municipalitiesregistry.model.RegistryPlaceCsvDTO;
 import it.municipalitiesregistry.model.RegistryPlaceDTO;
+import it.municipalitiesregistry.model.RegistryPlaceCsvDTO;
 import it.municipalitiesregistry.persistence.entity.RegistryPlaceEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -16,7 +16,6 @@ public interface RegistryPlaceMapper {
     @Mapping(target = "id.denominazioneInItaliano", source = "denominazioneInItaliano")
     @Mapping(target = "id.denominazioneUnitaTerritorialeSovracomunale", source = "denominazioneUnitaTerritorialeSovracomunale")
     @Mapping(target = "currentValid", constant = "true")
-    @Mapping(target = "uuidCode", expression = "java(java.util.UUID.randomUUID())")
     RegistryPlaceEntity dtoCsvToEntity(RegistryPlaceCsvDTO place);
 
     @Mapping(source = "id.denominazioneRegione", target = "denominazioneRegione")
