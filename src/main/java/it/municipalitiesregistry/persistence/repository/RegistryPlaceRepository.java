fd49ffdccda2e19dd6b3f182ffb7d38e313885ec
gioxi <desiderio.gio@yahoo.com>
2023-05-27 22:48:25 +0200

    update query

--- src/main/java/it/municipalitiesregistry/persistence/repository/RegistryPlaceRepository.java
+++ src/main/java/it/municipalitiesregistry/persistence/repository/RegistryPlaceRepository.java
@@ -3,19 +3,16 @@ package it.municipalitiesregistry.persistence.repository;
 import it.municipalitiesregistry.persistence.entity.RegistryPlaceEntity;
 import it.municipalitiesregistry.persistence.entity.RegistryPlaceId;
 import jakarta.persistence.LockModeType;
-import jakarta.persistence.Tuple;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Repository
 public interface RegistryPlaceRepository extends JpaRepository<RegistryPlaceEntity, RegistryPlaceId> {
@@ -29,9 +26,8 @@ public interface RegistryPlaceRepository extends JpaRepository<RegistryPlaceEnti
     @Query("SELECT place FROM RegistryPlaceEntity place where lower(place.id.denominazioneRegione) = lower(:regione) and lower(place.id.denominazioneUnitaTerritorialeSovracomunale) = lower(:provincia) and place.currentValid = true order by place.id.denominazioneInItaliano")
     List<RegistryPlaceEntity> findDistinctDenominazioneInItaliano(@Param("provincia") String denominazioneUnitaTerritorialeSovracomunale, @Param("regione") String denominazioneRegione);
 
-    Optional<RegistryPlaceEntity> findByIdCodiceCatastaleDelComune(String codiceCatastaleDelComune);
+    Optional<RegistryPlaceEntity> findByIdCodiceCatastaleDelComuneAndCurrentValidTrue(String codiceCatastaleDelComune);
 
-    @Transactional
     @Modifying
     @Query("update RegistryPlaceEntity place set place.currentValid = false, place.validTo = :dateTime where place.currentValid = true and place.lastUpdate < :dateTime")
     void updatePastOnes(@Param("dateTime") LocalDateTime dateTime);
