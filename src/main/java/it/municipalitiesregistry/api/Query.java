e03e94a1997aa29622f70ea4849d822273b7feab
gioxi <desiderio.gio@yahoo.com>
2023-05-28 01:23:27 +0200

    update query graphql resolver

--- src/main/java/it/municipalitiesregistry/api/Query.java
+++ src/main/java/it/municipalitiesregistry/api/Query.java
@@ -1,25 +1,32 @@
 package it.municipalitiesregistry.api;
 
 import graphql.kickstart.tools.GraphQLQueryResolver;
-import it.municipalitiesregistry.mapper.RegistryPlaceMapper;
 import it.municipalitiesregistry.model.RegistryPlaceDTO;
-import it.municipalitiesregistry.persistence.entity.RegistryPlaceEntity;
-import it.municipalitiesregistry.persistence.repository.RegistryPlaceRepository;
+import it.municipalitiesregistry.service.RegistryPlaceQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class Query implements GraphQLQueryResolver {
 
-    private final RegistryPlaceRepository registryPlaceRepository;
-    private final RegistryPlaceMapper mapper;
-
-    public RegistryPlaceEntity findEntityByMunicipalCode(String codiceCatastaleDelComune) {
-        return registryPlaceRepository.findByIdCodiceCatastaleDelComuneAndCurrentValidTrue(codiceCatastaleDelComune).orElseThrow();
-    }
+    private final RegistryPlaceQueryService registryPlaceQueryService;
 
     public RegistryPlaceDTO municipalityByCadastralCode(String codiceCatastaleDelComune) {
-        return mapper.fromEntityToResponseDto(findEntityByMunicipalCode(codiceCatastaleDelComune));
+        return registryPlaceQueryService.findByCadastralCode(codiceCatastaleDelComune);
+    }
+
+    public List<String> allRegions() {
+        return registryPlaceQueryService.getAllRegions();
+    }
+
+    public List<String> allProvincesByRegion(String region) {
+        return registryPlaceQueryService.getAllProvincesByRegion(region);
+    }
+
+    public List<RegistryPlaceDTO> allMunicipalitiesByProvinceAndRegion(String province, String region) {
+        return registryPlaceQueryService.getAllCitiesByProvinceAndRegion(province, region);
     }
 }
